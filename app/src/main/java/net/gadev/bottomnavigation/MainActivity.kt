f33ed7c0456f4346578522736ff760cf33625a61
Huy Van <mry.nsth@gmail.com>
2020-09-04 16:15:49 +0700

    part 1

---
+++ app/src/main/java/net/gadev/bottomnavigation/MainActivity.kt
@@ -0,0 +1,33 @@
+package net.gadev.bottomnavigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // binding view
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        // setup navigation
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_monster, R.id.navigation_slug
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+    }
+
+
+}
